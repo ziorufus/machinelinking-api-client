9961e59b9d694497f8729042eec137cbf96545f3
Michele Mostarda <michele.mostarda@gmail.com>
2013-12-15 17:50:40 +0100

    First client release.

---
+++ src/main/java/com/machinelinking/api/client/Clazz.java
@@ -0,0 +1,61 @@
+/*
+ *  Copyright 2013 MachineLinking S.r.l.
+ *
+ *  Licensed under the Apache License, Version 2.0 (the "License");
+ *  you may not use this file except in compliance with the License.
+ *  You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *  Unless required by applicable law or agreed to in writing, software
+ *  distributed under the License is distributed on an "AS IS" BASIS,
+ *  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *  See the License for the specific language governing permissions and
+ *  limitations under the License.
+ */
+
+package com.machinelinking.api.client;
+
+import java.net.URL;
+
+/**
+ * Defines a classification based on a <i>Wikipedia</i> class.
+ *
+ * @author Michele Mostarda (michele@machinelinking.com)
+ */
+public class Clazz {
+
+    private final String label;
+    private final URL url;
+    private final String resouce;
+    private final double prob;
+
+    public Clazz(String label, URL url, String resource, double prob) {
+        this.label = label;
+        this.url = url;
+        this.resouce = resource;
+        this.prob = prob;
+    }
+
+    public String getLabel() {
+        return label;
+    }
+
+    public URL getUrl() {
+        return url;
+    }
+
+    public String getResource() {
+        return resouce;
+    }
+
+    public double getProb() {
+        return prob;
+    }
+
+    @Override
+    public String toString() {
+        return String.format("label: [%s], url: %s, resource: %s, prob: %f", label, url, resouce, prob);
+    }
+
+}
---
+++ src/main/java/com/machinelinking/api/client/Image.java
@@ -0,0 +1,49 @@
+/*
+ *  Copyright 2013 MachineLinking S.r.l.
+ *
+ *  Licensed under the Apache License, Version 2.0 (the "License");
+ *  you may not use this file except in compliance with the License.
+ *  You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *  Unless required by applicable law or agreed to in writing, software
+ *  distributed under the License is distributed on an "AS IS" BASIS,
+ *  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *  See the License for the specific language governing permissions and
+ *  limitations under the License.
+ */
+
+package com.machinelinking.api.client;
+
+import java.net.URL;
+
+/**
+ * Defines an image associated to a resource.
+ *
+ * @author Michele Mostarda (michele@machinelinking.com)
+ */
+public class Image {
+
+    private final URL image;
+    private final URL thumb;
+
+    public Image(URL image, URL thumb) {
+        this.image = image;
+        this.thumb = thumb;
+    }
+
+    public URL getImage() {
+        return image;
+    }
+
+    public URL getThumb() {
+        return thumb;
+    }
+
+    @Override
+    public String toString() {
+        return String.format("image: %s, thumb: %s", image, thumb);
+    }
+
+}
